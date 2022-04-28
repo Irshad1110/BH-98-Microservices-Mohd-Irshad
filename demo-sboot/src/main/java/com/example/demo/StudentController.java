01a309abcd7b9d18be1516ebc9bd7a446cadad15
Mohd Irshad <mohd_irshad@thbs.india.com>
2022-04-28 16:03:40 +0530

    28thApril

--- demo-sboot/src/main/java/com/example/demo/StudentController.java
+++ demo-sboot/src/main/java/com/example/demo/StudentController.java
@@ -1,18 +1,33 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class StudentController {
 	
-	@RequestMapping("/student/{name}/{id}/{cmarks}/{pmarks}/{mmarks}")
-	String myStudents(@PathVariable String name, @PathVariable int id, @PathVariable int cmarks, @PathVariable int pmarks, @PathVariable int mmarks) {
-		Student s = new Student(id,name,pmarks,cmarks,mmarks);
-		String grade = s.getGrade();
-		return "Hello, My name is " +name + " and my id is " + id + " and my grade is " + grade; 
+	
+private final StudentRepository studentRepository;
+	
+	StudentController(StudentRepository studentRepository){
+		this.studentRepository=studentRepository;
 	}
+
+	@GetMapping("/students")
+	List<Student> getAll(){
+		return studentRepository.findAll();
+	}
+	
+//	@RequestMapping("/student/{name}/{id}/{cmarks}/{pmarks}/{mmarks}")
+//	String myStudents(@PathVariable String name, @PathVariable int id, @PathVariable int cmarks, @PathVariable int pmarks, @PathVariable int mmarks) {
+//		Student s = new Student(id,name,pmarks,cmarks,mmarks);
+//		String grade = s.getGrade();
+//		return "Hello, My name is " +name + " and my id is " + id + " and my grade is " + grade; 
+//	}
 	
 	
 
